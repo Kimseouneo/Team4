a8969a14db6a45eadb06e1183fd9c5c6e8bd58aa
Kimseouneo <98071412+Kimseouneo@users.noreply.github.com>
2025-12-06 17:15:18 +0900

    턴제 문제는 해결
    player2가 탄을 쏘고 난 후 바로 player1에게 턴이 넘어감
    아마 스크립트가 달라서 생기는 문제로 추정

--- character1/turnmanager.gd
+++ character1/turnmanager.gd
@@ -10,6 +10,19 @@ enum State { PLAYER1_TURN, PLAYER2_TURN }
 var state = State.PLAYER1_TURN
 var turn_timer = null
 func _ready():
+	#오류 방지를 위한 초기화
+	_end_turn(player1, bullet1)
+	_end_turn(player2, bullet2)
+	# 타이머 한 번만 생성 & 껐다가 키는 방식
+	turn_timer = Timer.new()
+	turn_timer.wait_time = 5.0   # 5초
+	turn_timer.one_shot = false  # 반복
+	add_child(turn_timer)
+	turn_timer.timeout.connect(next_turn)
+	turn_timer.start()
+
+	# 첫 턴 시작
+	state = State.PLAYER1_TURN	
 	_start_turn(player1, bullet1)
 
 func _start_turn(player, bullet):
@@ -17,31 +30,23 @@ func _start_turn(player, bullet):
 	bullet.set_active(true)
 	print(player.name, "'s TURN START")
 
-#턴 9초로 제한
-	if turn_timer:
-		turn_timer.queue_free()
-	turn_timer = Timer.new()
-	turn_timer.wait_time = 5.0
-	turn_timer.one_shot = true
-	add_child(turn_timer)
-	turn_timer.start()
-	turn_timer.timeout.connect(next_turn)
+func _end_turn(player, bullet):
+	player.set_active(false)
+	bullet.set_active(false)
+	if player == player1:
+		bullet1.global_position = player1.get_node("Anchor").global_position + Vector2(50, -10)
+	if player == player2:
+		bullet2.global_position = player2.get_node("Anchor").global_position + Vector2(-50, -10)
 	
 func next_turn():
 	if state == State.PLAYER1_TURN:
-		player1.set_active(false)
-		bullet1.set_active(false)
-		
-		player2.set_active(true)
-		bullet2.set_active(true)
+		_start_turn(player2, bullet2)
+		_end_turn(player1, bullet1)
 		state = State.PLAYER2_TURN
 		print("Turn → Player 2")
 	
 	else:
-		player2.set_active(false)
-		bullet2.set_active(false)
-		
-		player1.set_active(true)
-		bullet1.set_active(true)
+		_start_turn(player1, bullet1)
+		_end_turn(player2, bullet2)	
 		state = State.PLAYER1_TURN
 		print("Turn → Player 1")
