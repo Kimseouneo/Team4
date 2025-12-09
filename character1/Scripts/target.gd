b655ed6465a14f88729f39d86a6a93f53d9669fc
hwc1114 <adamchoi1114@ajou.ac.kr>
2025-12-09 11:09:11 +0900

    target.gd 수정

--- character1/Scripts/target.gd
+++ character1/Scripts/target.gd
@@ -1,14 +1,17 @@
 extends StaticBody2D
 
-@onready var score_text: TextEdit = $"../Score"
+@onready var score_text: Panel = $"../Score"
+@onready var score_label: Label = $"../Score/ScoreLabel"
 @onready var bullet = $"../char_silver/bullet"
 var score = 0
 
 func _on_bullet_body_entered(body: Node) -> void:
 	if body.name == "Target":
 		add_score(1)
+		show_message("Score: " + str(score))
 
 func add_score(points: int):
 	score += points
-	if score_text:
-		score_text.text = "Score: " + str(score)
+
+func show_message(text: String) -> void:
+	score_label.text = text 
